6e5a123ce946dcc253285abb346f0564a3fb128f
Fabiano <fabiano.pereira@seventh.com.br>
2022-03-13 14:40:46 -0300

    add authorization on map

--- PokemonGO/ViewController.swift
+++ PokemonGO/ViewController.swift
@@ -8,15 +8,45 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapa: MKMapView!
+    var gerenciadorLocalizacao = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        mapa.delegate = self
+        gerenciadorLocalizacao.delegate = self
+        gerenciadorLocalizacao.requestWhenInUseAuthorization()
+        gerenciadorLocalizacao.startUpdatingLocation()
+        
+        
     }
 
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if status != .authorizedWhenInUse && status != .notDetermined {
+            //alerta
+            let alertController = UIAlertController(title: "Permissão de localização", message: "Para que você possa caçar pokemons, precisamos da sua localização!! Por favor habilite", preferredStyle: .alert)
+            
+            let acaoConfiguracoes = UIAlertAction(title: "Abrir Configurações", style: .default, handler: {
+                (alertConfiguracoes) in
+                
+                if let configuracoes = NSURL(string: UIApplication.openSettingsURLString) {
+                    UIApplication.shared.open(configuracoes as URL)
+                }
+            })
+            
+            let acaoCancelar = UIAlertAction(title: "Cancelar", style: .default, handler: nil)
+            
+            alertController.addAction(acaoConfiguracoes)
+            alertController.addAction(acaoCancelar)
+            
+            present(alertController, animated: true, completion: nil)
+            
+        }
+    }
 
 }
 
